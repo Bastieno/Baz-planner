1175d8d65934e0af9b404f50a3393bb621d8bdbb
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-11-23 22:52:34 +0100

    setup the folder structure of the app using vue cli, create the base app component and the activity-item component

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    'no-console': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/max-attributes-per-line': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'vue/multiline-html-element-content-newline': 0
+  }
+}
